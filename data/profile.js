dd0be24b2f4f065cfa614a0ec790bc4bd6f92029
muserprateek <prateek.k@travelyaari.com>
2021-11-08 21:26:45 +0530

    small change

--- data/profile.js
+++ data/profile.js
@@ -1,142 +1,141 @@
 const profileData = {
-    "title": "Profile",
-    "name": "Prateek Kumar",
-    "sub_title": "B. Tech undergraduate (Computer Science and Engineering)",
-    "about": {
-        "intro": "Skilled CodeIgniter full stack web developer eager to join a creative, problem solving team. Ability to learn and implement new technologies quickly. My skills include CodeIgniter, php, HTML5, CSS3, Javascript, jQuery, PHP, React.js. These skills were gained and polished during my working experince as full stack developer for two years",
-        "contact": {
-            "email": "prateekkmr41@gmail.com",
-            "phone": "+918853849950",
-            "address": "562, T.R Muniswamappa Building,1st Main 5th cross ,BENGALURU ,KARNATAKA-560066",
-            "portfolio": "http://resume.prateekkumar.net/"
-        }
+    title: "Profile",
+    name: "Prateek Kumar",
+    sub_title: "B. Tech undergraduate (Computer Science and Engineering)",
+    about: {
+        intro: "Frontend end developer,UI/UX, eager to join a creative, problem solving team. Ability to learn and implement new technologies quickly. My skills include React.js,redux,scss,thunk,html,css,javascript, CodeIgniter, php. These skills were gained and polished during my working experince as full stack developer for two years",
+        contact: {
+            email: "prateekkmr41@gmail.com",
+            phone: "+918853849950",
+            address:
+                "562, T.R Muniswamappa Building,1st Main 5th cross ,BENGALURU ,KARNATAKA-560066",
+            portfolio: "http://www.prateekkumar.net",
+        },
     },
-    "links": [
-        { "title": "GitHub", "src": "https://github.com/userprateek" },
-        { "title": "HackerRank", "src": "https://www.hackerrank.com/prateekkmr41" }, 
-        { "title": "LinkedIn", "src": "https://www.linkedin.com/in/userprateek" }
+    links: [
+        { title: "GitHub", src: "https://github.com/userprateek" },
+        { title: "HackerRank", src: "https://www.hackerrank.com/prateekkmr41" },
+        { title: "LinkedIn", src: "https://www.linkedin.com/in/userprateek" },
     ],
-    "education": [
+    education: [
         {
-            "alma": "Faculty of Engineering and Technology, GKV, Haridwar",
-            "duration": "2014 - 2018", "std": "B. Tech (CSE)",
-            "score": "63.60%"
+            alma: "Faculty of Engineering and Technology, GKV, Haridwar",
+            duration: "2014 - 2018",
+            std: "B. Tech (CSE)",
+            score: "63.60%",
         },
         {
-            "alma": "Jawahar Navodaya Vidyalaya, jaunpur, Uttar pradesh",
-            "duration": "2012 - 2014", "std": "Class XI - XII",
-            "score": "81.49%"
+            alma: "Jawahar Navodaya Vidyalaya, jaunpur, Uttar pradesh",
+            duration: "2012 - 2014",
+            std: "Class XI - XII",
+            score: "81.49%",
         },
         {
-            "alma": "Jawahar Navodaya Vidyalaya, jaunpur, Uttar pradesh",
-            "duration": "2006 - 2011", "std": "Class VI - X",
-            "score": "9.0CGPA"
-        }
+            alma: "Jawahar Navodaya Vidyalaya, jaunpur, Uttar pradesh",
+            duration: "2006 - 2011",
+            std: "Class VI - X",
+            score: "9.0CGPA",
+        },
     ],
-    "skills": [
+    skills: [
         {
-            "graph": "false",
-            "category": "Programming Languages",
-            "topics": [
-                "php","JavaScript","HTML / CSS","C++"
-            ]
+            graph: "false",
+            category: "Frontend Frameworks & Lib",
+            topics: ["React.js", "Sass", "HTML5"],
         },
         {
-            "graph": "false",
-            "category": "Backend Frameworks & Lib",
-            "topics": [
-                "CodeIgniter", "Node.js"
-            ]
+            graph: "false",
+            category: "Programming Languages",
+            topics: ["JavaScript", , "HTML / CSS", "php"],
         },
         {
-            "graph": "false",
-            "category": "Frontend Frameworks & Lib",
-            "topics": [
-                "React.js", "Sass"
-            ]
+            graph: "false",
+            category: "Backend Frameworks & Lib",
+            topics: ["CodeIgniter", "Node.js"],
         },
         {
-            "graph": "false",
-            "category": "Database",
-            "topics": [
-                "SQL bases (mainly MYSQL)"
-            ]
+            graph: "false",
+            category: "Database",
+            topics: ["SQL(mySQL)", "mondodb(novice)"],
         },
         {
-            "graph": "false",
-            "category": "Cloud Services",
-            "topics": [
-                "Amazon Web Services"
-            ]
+            graph: "false",
+            category: "Cloud Services",
+            topics: ["Amazon Web Services"],
         },
         {
-            "graph": "false",
-            "category": "Others",
-            "topics": [
-                "Git","Postman","VS Code","Data structure and Algorithms","Apache server","AJAX"
-            ]
-        }
-        
+            graph: "false",
+            category: "Others",
+            topics: [
+                "Git",
+                "Postman",
+                "VS Code",
+                "Apache server",
+                "AJAX",
+                "heroku",
+            ],
+        },
     ],
-    "projects": [{
-        "title": "Microsite website builder development ",
-        "duration": "November 2019 – February 2020",
-        "link": "",
-        "tech": "Technologies used – php ,Codeigniter, html, css, React,angular, mysql",
-        "desc": "A PHP based application which can generate websites using certain templates. this application takes all the data to be there on website create website. Also supports modification. consist of an admin page and theme based public page. Admin page can modify look and feel of public pages"
-    },
-    {
-        "title": "Ticket booking flow in websites",
-        "duration": "February 2020 – April 2020",
-        "link": "",
-        "tech": "Technologies used – React.js, Sass, REST API",
-        "desc": "A react powered application which enables users to search for a trip on bus ticket booking website, select their seats, fill their details and book the ticket into system. This application uses a backend named as computerized reservation system. All the communication to computerized reservation system are done using REST api. Frontend is react application"       
-    },
-    {
-        "title": "Theme development for websites",
-        "duration": "April 2020 – June 2020 ",
-        "link": "",
-        "tech": "Technologies used – html, css , javascript , php, mysql",
-        "desc": "A fronted development in which I created UI/UX for website which consist of home-page, login-page and user profile page and other several pages. In this project I worked in fronted as well as integration to backend. This theme could be used in website builder projects or as stand alone application."       
-    },
-    {
-        "title": "Poller cronjob to handle Incomplete orders",
-        "duration": "June 2020 – July 2020",
-        "link": "",
-        "tech": "Technologies used – php, codeigniter, mysql, REST API",
-        "desc": "A backend script to handle orders for which payment gateway could not send a proper response at the time of payment or users connection lost before getting redirect back to merchant website after making payment at payment gateway. This was a cronjob script so I used to be running every two minutes and this script can fetch payment status from payment gateway and complete the order without any user intervention"       
-    }
-    ],
-    "experiences": [
-	{
-            "desc": "<b> Software Engineer </b> at <a target='_blank' href='http://www.mantistechnologies.com/'> <strong> Mantis Technologies </strong> </a>",
-            "date": "October 24,2019 - August 31,2021",
-            "details": [
-               "As a software engineer I have worked in around 6 projects so far. And lot of small task in other projects.",
-               "I have been constantly and comfortably juggling between technologies and have been successfully generating satisfactory results for the organization with each project/assignment I am charged with.",
-               " I have experience of Payment gateway integration of payu, aggrepay, paynimo.",
-               "I have experience in AWS hosting including AWS s3, AMI and elastic IP.",
-               " I have experience in managing LAMP server and SSL integration into apache server."
-            ]
-        }
-    ],
-    "events": [
+    projects: [
         {
-            "title": "certifications",
-            "body": [
+            title: "website builder development ",
+            duration: "November 2019 – February 2020",
+            link: "",
+            tech: "Technologies used – php ,Codeigniter, html, css, React,angular, mysql",
+            desc: "A PHP based application which can generate websites using certain templates. this application takes all the data to be there on website create website. Also supports modification. consist of an admin page and theme based public page. Admin page can modify look and feel of public pages",
+        },
+        {
+            title: "Ticket booking Feature in websites",
+            duration: "February 2020 – july 2020",
+            link: "",
+            tech: "Technologies used – React.js, Sass, REST API",
+            desc: "A react powered application which enables users to search for a trip on bus ticket booking website, select their seats, fill their details and book the ticket into system. This application uses a backend named as computerized reservation system(CRS).this web app communicate with backend using ajax/rest api calls",
+        },
+        {
+            title: "Ticket modication feature in websites",
+            duration: "May 2020 – October 2020",
+            link: "",
+            tech: "Technologies used – React.js, Sass, REST API",
+            desc: "A react powered application which enables users to modify their existing ticket details,such as modifying passenger information to reschduling the journey date,Including partially cancelling the tickets.  This application uses a backend named as computerized reservation system(CRS).this web app communicate with backend using ajax/rest api calls",
+        },
+        {
+            title: "Theme development for websites",
+            duration: "October 2020 – February 2021 ",
+            link: "",
+            tech: "Technologies used – html, css , javascript , php, mysql",
+            desc: "A fronted development in which I created UI/UX for website which consist of home-page, login-page and user profile page and other several pages. In this project I worked in fronted as well as integration to backend. This theme could be used in website builder projects or as stand alone application.",
+        },
+    ],
+    experiences: [
+        {
+            desc: "<b> Software Engineer </b> at <a target='_blank' href='http://www.mantistechnologies.com/'> <strong> Mantis Technologies </strong> </a>",
+            date: "October 24,2019 - August 31,2021",
+            details: [
+                "As a software engineer I have worked in around 6 projects so far. And lot of small task in other projects.",
+                "I have been constantly and comfortably juggling between technologies and have been successfully generating satisfactory results for the organization with each project/assignment I am charged with.",
+                " I have experience of Payment gateway integration of payu, aggrepay, paynimo.",
+                "I have experience in AWS hosting including AWS s3, AMI and elastic IP.",
+                " I have experience in managing LAMP server and SSL integration into apache server.",
+            ],
+        },
+    ],
+    events: [
+        {
+            title: "certifications",
+            body: [
                 {
-                    "desc": "a online learning course in <strong> Javascript (Basic) Certificate </strong> hosted by <b> Hackerrank </b>",
-                    "date": "10:Sept, 2020"
+                    desc: "a online learning course in <strong> Javascript (Basic) Certificate </strong> hosted by <b> Hackerrank </b>",
+                    date: "10:Sept, 2020",
                 },
                 {
-                    "desc": "a certificate in <strong> Programming, Data Structures and Algorithms using Python </strong> conducted by <b>NPTEL</b>",
-                    "date": "March, 2018"
+                    desc: "a certificate in <strong> Programming, Data Structures and Algorithms using Python </strong> conducted by <b>NPTEL</b>",
+                    date: "March, 2018",
                 },
                 {
-                    "desc": "a training program in <strong> Python </strong> conducted by <b>IIT Bombay</b>funded by<b>MHRD,India Government</b>",
-                    "date": "December, 2017"
-                }
-            ]
-        }
-    ]
-}
+                    desc: "a training program in <strong> Python </strong> conducted by <b>IIT Bombay</b>funded by<b>MHRD,India Government</b>",
+                    date: "December, 2017",
+                },
+            ],
+        },
+    ],
+};
